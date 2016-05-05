372b7d2e975ff4a33705379332d29f69f8bb9ab0
svtter <svtter@qq.com>
2016-05-05 12:08:07 +0800

    update learn

--- example4/learn.py
+++ example4/learn.py
@@ -1,8 +1,8 @@
-
+#!/usr/bin/env python3
 
 import sys
 sys.path.insert(0, "../")
-import codecs
+# import codecs
 import shelve
 
 from aiml.LangSupport import mergeChineseSpace
@@ -28,11 +28,18 @@ category_template = """
 </category>
 """
 
-#print sys.argv
+print(sys.argv)
+# TODO: need to rewrite
+'''
+因为是分词后的结果，并不一定是三个词
+'''
+
 if len(sys.argv) == 3:
     _, rule, temp = sys.argv
+    print(_, rule, temp)
     rule = mergeChineseSpace(str(rule, 'utf8'))
     temp = mergeChineseSpace(str(temp, 'utf8'))
+    print(_, rule, temp)
     db[rule] = temp
     db.sync()
     rules = []
