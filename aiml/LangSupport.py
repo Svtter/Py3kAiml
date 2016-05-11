498801caaf2ba87a5f7fcceadfe523cd64a695b9
svtter <svtter@qq.com>
2016-05-11 14:36:12 +0800

    update

--- aiml/LangSupport.py
+++ aiml/LangSupport.py
@@ -1,7 +1,6 @@
 # encoding: utf-8
 
 import jieba
-import logging
 
 
 # 立即初始化jieba
@@ -10,19 +9,19 @@ jieba.initialize()
 
 __author__ = 'svtter'
 
+
 def splitChinese(sentence, cutAll=False):
     '''
     cutall为false为精确模式，即分词合并还是句子
     '''
-
     # TODO: 标点符号分字
-    seg_list = jieba.lcut(sentence, cut_all = cutAll)
-    punct = set(u''':!),.:;?]}¢'"、。〉》」』】〕〗〞︰︱︳﹐､﹒
-    ﹔﹕﹖﹗﹚﹜﹞！），．：；？｜｝︴︶︸︺︼︾﹀﹂﹄﹏､～￠
-    々‖•·ˇˉ―--′’”([{£¥'"‵〈《「『【〔〖（［｛￡￥〝︵︷︹︻
-    ︽︿﹁﹃﹙﹛﹝（｛“‘-—…''')
-    filterpuntl = lambda l: list(filter(lambda x: x not in punct, l))
-    seg_list = filterpuntl(seg_list)
+    seg_list = jieba.lcut(sentence, cut_all=cutAll)
+    # punct = set(u''':!),.:;?]}¢'"、。〉》」』】〕〗〞︰︱︳﹐､﹒
+    # ﹔﹕﹖﹗﹚﹜﹞！），．：；？｜｝︴︶︸︺︼︾﹀﹂﹄﹏､～￠
+    # 々‖•·ˇˉ―--′’”([{£¥'"‵〈《「『【〔〖（［｛￡￥〝︵︷︹︻
+    # ︽︿﹁﹃﹙﹛﹝（｛“‘-—…''')
+    # filterpuntl = lambda l: list(filter(lambda x: x not in punct, l))
+    # seg_list = filterpuntl(seg_list)
     mid = ' '.join(seg_list)
     after = list()
     after.append(mid)
